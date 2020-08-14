fea1525f009ed644f96a652c1bcd66adf235a9d2
Christine Martino <christine.martino@thinkcompany.com>
2020-08-13 21:13:51 -0400

    Add handlebars package

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Configure our application
+// Tell express where our static assets are
+app.use(express.static(__dirname + "/public"));
+
+// Set ejs as templating language
 
 // Set the routes
 app.use(require("./app/routes"));
