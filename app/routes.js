fbb6dc65cb06a74c054e9a24e90791c260afbfc7
Christine Martino <christine.martino@thinkcompany.com>
2020-08-20 10:30:46 -0400

    mapping out future routes

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,15 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("./controllers/main.controller");
 
+// Main routes (Bag)
 router.get("/", mainController.showHome);
+
+// Item detail routes
 router.get("/item/:slug", mainController.showItem);
+
+// Create Item
+// Edit Item
+// Delete Item
 
 //Export the router
 module.exports = router;
