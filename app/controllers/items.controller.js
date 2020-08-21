74ef8e6678d34681bc5e588ee9d07ffa2f0fea77
Christine Martino <christine.martino@thinkcompany.com>
2020-08-21 18:29:27 -0400

    Refactor Items controller

--- app/controllers/items.controller.js
+++ app/controllers/items.controller.js
@@ -1,29 +1,33 @@
 const Item = require("../models/item");
+const items = require("../../data/items.json").results;
 
 const getItem = query => items.results.find(item => item.slug === query);
 
+// Show all Items
+function showItems(req, res) {
+  res.render("items", { items });
+}
+
+// Show single Item
+function showItem(req, res) {
+  res.render("item", { item: getItem(req.params.slug) });
+}
+
+function seedItems(req, res) {
+  // Use the item model to insert/save
+  Item.remove({}, () => {
+    for (item of items) {
+      let newItem = new Item(item);
+      newItem.save();
+    }
+  });
+
+  // Seeded!
+  res.send("Database Seeded!");
+}
+
 module.exports = {
-  showItems: (req, res) => {
-    res.render("items", { items });
-  },
-  showItem: (req, res) => {
-    res.render("item", { item: getItem(req.params.slug) });
-  },
-
-  // Seed our database
-  seedItems: (req, res) => {
-    // Create some items from static file
-    const items = require("../../data/items.json").results;
-
-    // Use the item model to insert/save
-    Item.remove({}, () => {
-      for (item of items) {
-        let newItem = new Item(item);
-        newItem.save();
-      }
-    });
-
-    // Seeded!
-    res.send("Database Seeded!");
-  }
+  showItems: showItems,
+  showItem: showItem,
+  seedItems: seedItems
 };
